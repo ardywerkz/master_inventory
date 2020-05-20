cfef9f559b52a8e9142797d2a08ffc0fd8a6b6cc
ardy <diazardy549@gmail.com>
2020-05-20 15:05:09 +0800

    db connect

--- application/config/database.php
+++ application/config/database.php
@@ -76,9 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'inventory',
+	'username' => 'modupack_inventory',
+	'password' => 'vljx?2Jo=NIk',
+	'database' => 'modupack_inventory',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
